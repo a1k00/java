f6f7d18c009e2541150870aae7185b5fdaf901a7
aiko <88936437+a1k00@users.noreply.github.com>
2021-08-25 12:11:17 +0300

    Add files via upload

---
+++ javaknow/anim.js
@@ -0,0 +1,12 @@
+ let momi=anime({
+  targets: '#dove',
+  translateX: 1200,
+  autoplay:false,
+ /* loop:true,*/
+  duration:7000,
+  borderRadius:'50%',
+  rotate:'3turn'
+});
+
+
+ document.querySelector('.chik').onclick=momi.restart; 
---
+++ javaknow/anime.js
@@ -0,0 +1,127 @@
+let hello=(13)
+console.log(typeof hello);
+console.log(hello);
+
+
+
+let bye= 55===55;
+console.log(bye)
+
+
+let opp='byeeeeeeeeeeeee'
+console.log(opp)
+
+console.log('sikee')
+
+let obj={
+	color:'brown',
+	color1:'black',
+	color3:'yellow',
+	color4:'red'
+}
+console.log(obj);
+
+
+
+let meesage='nothing cool'
+let message='nothing bad'
+let auf =5521;
+let ikeia=234567856;
+if (auf>ikeia) {
+	console.log(meesage)
+}else{
+	console.log(message)
+}
+
+let hii=['red','yellow','black','pink' ]
+console.log(hii[3]);
+
+ hii[4]='green';
+ console.log(hii)
+
+
+ hii[0]='white';
+let hiii=['red','yellow','black','pink' ]
+hiii.shift();
+console.log(hiii)
+
+
+
+
+
+
+
+
+
+
+
+let hi=['red','yellow','black','pink' ]
+hi.splice(1,3);
+console.log(hi);
+
+
+
+let hello1=['red','yellow','black','pink' ]
+hello1.splice(1,2, 'silver');
+console.log(hello1);
+
+
+
+let hello2=['red','yellow','black','pink' ]
+console.log(hello2.includes('black'))
+
+
+
+
+let oof=['red','yellow','black','pink' ]
+console.log(oof.sort());
+
+
+let iarOof=[2,5555555,3,484,88,1999,560]
+console.log(iarOof.sort((a,b)=>a-b));
+
+
+let emotion=(name,lastName)=>name+' '+lastName;
+console.log(emotion('katea','ion'))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function annie(x,y){
+let colorScroll=x+y;
+console.log(colorScroll);
+}
+
+annie(5,5);
+
+
+
+
+function maddy(x,y){
+	let colorspring=x/y;
+	console.log(colorspring);
+}
+maddy(20,4)
+
+
+
+
+
+
+
+
+
+
